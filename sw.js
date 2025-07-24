89c7f4448713cbd3db1f3b90a6a4cd9899a35afb
Yeliva Dinanti <76162598+yeliva@users.noreply.github.com>
2025-07-24 14:05:58 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'faktur-roviana-v1';
-// Daftar semua file yang dibutuhkan agar aplikasi bisa berjalan offline
+
 const urlsToCache = [
-  '.', // Mewakili file HTML utama
+  '.', 
   'manifest.json',
   'https://cdn.tailwindcss.com',
   'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
@@ -9,7 +9,7 @@ const urlsToCache = [
   'https://cdn.jsdelivr.net/npm/flatpickr'
 ];
 
-// Proses instalasi: menyimpan file ke cache
+
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -20,16 +20,16 @@ self.addEventListener('install', event => {
   );
 });
 
-// Proses fetch: mencegat permintaan jaringan
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Jika ada di cache, langsung berikan dari cache
+        
         if (response) {
           return response;
         }
-        // Jika tidak ada, ambil dari jaringan
+        
         return fetch(event.request);
       })
   );
